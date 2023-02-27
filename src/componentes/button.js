5253f0e50596c5fc114a4d2b10b011c9f8e707a9
YNK0 <francisco.vargasmagana03@gmail.com>
2023-02-27 17:21:06 -0600

    Updates

--- src/componentes/button.js
+++
@@ -1,9 +0,0 @@
-import "../App.css"
-
-function Button(props) {
-    return(
-        <li><a className="contact-button" href="#"><button className="btn">{props.children}</button></a></li>
-    )
-}
-
-export default Button;
--- src/componentes/images.jsx
+++
@@ -1,9 +0,0 @@
-import img1 from "../images/img-1.png";
-import img2 from "../images/img-2.png";
-import img3 from "../images/img-3.png";
-import img4 from "../images/img-4.png";
-import img5 from "../images/img-5.png";
-import img6 from "../images/img-6.png";
-import img7 from "../images/img-7.png";
-
-export default [img1, img2, img3, img4, img5, img6, img7]
